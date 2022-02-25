5a6309b1dfa7434c7e4f1c800bf49e5ad20b3c8e
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-25 06:31:33 +0000

    npm installed body-parser and implemented it in POST /urls_new

--- express_server.js
+++ express_server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Functions
+function generateRandomString() {
+  let randomString = '';
+  // alphanumericChars are A-Z capital a-z lowercase and 0-9
+  let alphaNumericChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrdtuvwxyz";
+  while(randomString.length < 6) {
+    randomString += alphaNumericChars.charAt((Math.ceil(Math.random() * alphaNumericChars.length)));
+  };
+  return randomString;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,7 +33,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
@@ -29,6 +47,12 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+//Posts
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
