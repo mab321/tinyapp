a8c96463b4a5b9858ec68758dc14ecec58f37c4f
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-25 07:27:35 +0000

    Added /u/:shortURL route handling

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Functions
 function generateRandomString() {
-  let randomString = '';
+  let randomString = "";
   // alphanumericChars are A-Z capital a-z lowercase and 0-9
   let alphaNumericChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrdtuvwxyz";
   while(randomString.length < 6) {
@@ -45,6 +45,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  if (urlDatabase[shortURL]) {
+    const longURL = urlDatabase[shortURL];
+    res.redirect(longURL);
+  } else {
+    res.send("404 URL no Found");
+  }
+  
+  
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -52,7 +64,10 @@ app.get("/urls.json", (req, res) => {
 //Posts
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  
+  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
